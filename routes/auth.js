df2583b8bca1f87583719ff43a0e51ecc93cd1ea
sahmed <sahmed@alumni.depaul.edu>
2019-09-09 18:47:43 -0500

    middleware done, private routes for getting logged in user working

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,25 @@
 // registring users routes
+//we need Being able to login to get a token as well.
+//Login authentication route is going to go in our outh.js.
 // bringing express so that we can use the router
 const express = require("express");
 // create a variable router and set that to express.Router
 const router = express.Router();
 
+const bcrypt = require("bcryptjs");
+// bringing JSON Web Token
+const jwt = require("jsonwebtoken");
+// bringin config becuase it has jwt secret.
+const config = require("config");
+
+// bringing middleware to protect a route
+const auth = require("../middleware/auth");
+
+// (npm install --save express-validator). requiring
+const { check, validationResult } = require("express-validator");
+
+// bringing User model
+const User = require("../models/User");
 //instead of app.post or get we are going to do router.post.
 /**
  * post request is for submitting data to the server
@@ -16,19 +32,81 @@ const router = express.Router();
 // @route GET api/auth (route, type of request, endpoint) Now '/' pertains to api/users
 // @desc Get logged in user
 // @access Private (getting a user who is logged in so it is private)
+// this is a private protected route. anytime we need to protect a route, we need to bring in the middleware
+// pass it as a second parameter
 
-router.get("/", (req, res) => {
-  res.send("Get logged in user");
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password"); // send NO password
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error!");
+  }
 });
 
 // Second Route is to actually login the user and get the token.
 // @route POST api/auth ( sending data to get authenticated)
 // @desc Auth user and get token
 // @access Public (so that u can access the private routes)
+// validation to check if there is an email and a password sent.
+router.post(
+  "/",
+  [
+    check("email", "Please Enter a Valid Email").isEmail(),
+    check("password", "Password is required").exists()
+  ],
+  async (req, res) => {
+    //if there is error send this response
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // taking email and passowrd out of the body
+    const { email, password } = req.body;
+    // doing a try and catch. check by email if a user exists. User.findOne returns a promise.
+    // if there is not a user then we want to send back an error that invalid credentials.
 
-router.post("/", (req, res) => {
-  res.send("Log in user");
-});
+    try {
+      let user = await User.findOne({ email });
+
+      // if there is no user with that email then let's
+      //return a status of 400 and send back some JASON that says "Invaild Credentials!"
+      if (!user) {
+        return res.status(400).json({ msg: "Invalid Credentials!" });
+      }
+      // if there is a user then we want to continue to check the password becuase up to this point
+      //we've only checked the email. create a variable called isMatch and set this to await because
+      //bcrypt. compare returns a promise. it takes plain text password (comes from the body) and
+      //the hash password (password stored in the database).
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      // they are gonna return true or false
+      if (!isMatch) {
+        return res.status(400).json({ msg: "Invalid Credentials!" });
+      }
+      // if password does match, then send the token.
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token }); // return the token
+        }
+      );
+    } catch (error) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 // exporting the router
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,12 @@
 const express = require("express");
 // create a variable router and set that to express.Router
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+// bringing JSON Web Token
+const jwt = require("jsonwebtoken");
+// bringin config becuase it has jwt secret.
+const config = require("config");
+
 // (npm install --save express-validator). requiring
 const { check, validationResult } = require("express-validator");
 
@@ -38,8 +44,86 @@ router.post(
       "Please Enter a Password with 6 or More Characters."
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
-    res.send(req.body);
+  async (req, res) => {
+    // res.send(req.body); 400 means bad data. user didn't fulfil the requirement.
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // request.body should have name, email and password. // destruturing
+
+    const { name, email, password } = req.body;
+
+    /*** ERROR CHECKING
+     * creating try/catch becuase we will be using sync await dealing with the database and also with bcrypt
+     * which also returns
+     * promises that their methods will return a promise.
+     *  first we want to check if there is a user with that email, if yes, then will will return a message
+     * saying 'user already exists.
+     * we can't use await unless we mark our function as sync
+     */
+
+    try {
+      // take our User Model and use Mongoose method called findOne().
+      // we can find a user based on any field. here we are going to use email.
+      // we can also write {email: email} since both are called email, we can just write email in ES6.
+      let user = await User.findOne({ email });
+      // want to check if a user exists. then we want to return a response in a json object with a msg. 400 status, bad request
+      if (user) {
+        return res.status(400).json({ msg: "Email already exists!" });
+      }
+      // if email doesn't exist then take the user variable that we initialized above and set it to new User.
+      // using User Model to create a new user. Pass in the fields name, email and password as object. {name: name, email...}
+      user = new User({
+        name,
+        email,
+        password
+      });
+      // got created but hasn't been saved in the database
+      // before we save it into the database we need to encrypt the password with bcrypt.
+      // needed to create a variable called salt in order to encrypt the password.
+      // use bcrypt method called GenSalt that will generate a salt. genSalt takes in number of rounds as parameter.
+      // it returns promises so we use sync await instead of dot then.
+
+      const salt = await bcrypt.genSalt(10);
+      // we can take that salt and hash the password.
+
+      user.password = await bcrypt.hash(password, salt);
+      // saving to the database
+      await user.save();
+      // res.send("User Saved!");
+      /***So now we can register a user and add them to our database,
+       * we want to respond with a special token. That’s going to have a payload in it.
+       * 	User is getting saved in the database but we want to be able to log in right away when
+       * we register within React frontend. In order to do that we need to get the tokens send. Basically,
+       * we need to sign a JASON web token and send it to the client so that
+       * they can store it and use that to access protected routes. */
+      // creating payload which is the object we want to send in the token.
+      // with this user id we can access all the contacts that the logged in user has.
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      // to generate a token we have to sign it. take JWT and call dot sign which takes in a couple prameters like
+      //payload, secret (create in the config file), third parameter is object of options.
+      //and a  callback parameter with possible err and the token itsef.
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 3600 // mean 1 hour. ater that time it will logout.
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error.");
+    }
   }
 );
 
